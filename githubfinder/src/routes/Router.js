b6fa59d3c3bd75b769e0b23f25d0f30172b0d406
alvesgarrido <matheus.agarrido@gmail.com>
2021-06-26 07:33:29 -0300

    Contrução do Header das páginas, utilizando Material UI

--- githubfinder/src/routes/Router.js
+++ githubfinder/src/routes/Router.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 import UserDetailsPage from '../pages/UserDetailsPage/UserDetailsPage'
 import ErrorPage from '../pages/ErrorPage/ErrorPage'
+import Header from '../components/Header/Header'
 
 export default function Router() {
     return (
         <BrowserRouter>
+            <Header />
             <Switch>
                 <Route exact path="/">
                     <HomePage/>
